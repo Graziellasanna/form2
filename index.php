eed9c1c8fc2057317d377a8eee1ea90769c67f4c
Graziellasanna <dancafg@gmail.com>
2017-05-22 12:11:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div class="jumbotron">
+  <div class="container">	
+<!--navbar-->
+<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-menu" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"></a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right collapse navbar-collapse" id="my-menu">
+					<li><a href="#">Storia</a></li>
+					<li><a href="#">Eventi</a></li>
+					<li><a href="#contact">Contattaci</a></li>
+				</ul>  
+				</div>  
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+	<div class="row">
+		<div class="col-md-12 welcome">
+			<h1>Novenario di San Michele Ghilarza</h1>
+			<h2>Villaggio al centro della Sardegna, venite a trovarci dal 4 al 14 maggio 2017</h2>
+		</div>
+	</div>
+  </div>
+</div>
+<div class="container-fluid" id="contact" >
+  <div class="row text-center">
+  <h1>Contattaci </h1>
+  <h5>Risponderemo presto</h5>
+</div>
+  <form class="sanna-form">
+   <div class="col-lg-12">
+   <div class="col-lg-6">
+	<div class="form-group">
+	<label for="exampleInputEmail1">Indirizzo email </label>
+	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Per favore, scrivi qui la tua email" required>
+  </div>
+  <div class="form-group">
+	<label for="exampleInputPassword1">Password</label>
+	<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Per favore, scrivi qui la tua password" required>
+  </div>
+   </div>
+	<div class="col-md-6">
+	<label for="exampleInputPassword1">Lascia un messaggio</label>
+  <textarea class="form-control" rows="3" required></textarea>
+  <div class="row">
+		<div class="col-lg-12 text-center">
+<button type="submit" class="btn btn-primary btn">Submit</button>
+</div> 
+</div>
+</div>
+</form>
+
+<!--form end-->
+
+<script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+<script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="vendors/main.js"></script>
+</body>
+</html>
